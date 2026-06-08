9a8d28103f566093154eeb17008f03297248673a
ɑlennn:3 <wpoke62@gmail.com>
2026-06-08 19:38:50 +0700

    Create yta.js

---
+++ plugins/download/yta.js
@@ -0,0 +1,58 @@
+export const run = {
+  cmd: ['ytmp3'],
+  hidden: ['yta'],
+  category: 'downloader',
+  run: async (ctx, { text }) => {
+    if (!text) {
+      return ctx.reply(`# Cara penggunaan\n> *${ctx.prefix + 'ytmp3'} https://youtu.be/PrF3E-otC_E*`)
+    }
+
+    const ytRegex = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/.+$/i
+
+    if (!ytRegex.test(text)) {
+      return ctx.reply(`# Cara penggunaan\n> *${ctx.prefix + 'ytmp3'} https://youtu.be/PrF3E-otC_E*`)
+    }
+
+    ctx.reply(config.msg.wait)
+
+    try {
+      const result = await Func.fetchJson(
+        `${config.api.baseUrl.skyzxu}/api/ytmp3?url=${encodeURIComponent(text)}`
+      )
+
+      if (!result.success || !result.results) {
+        return ctx.reply(Func.jsonFormat(result))
+      }
+
+      const { metadata, download } = result.results
+
+      if (metadata.duration > 3600) {
+        return ctx.reply('❌ Durasi audio lebih dari 60 menit.')
+      }
+
+      const caption =
+        `#> YouTube Download\n` +
+        `- title : ${metadata.title || ''}\n` +
+        `- uploader : ${metadata.uploader || ''}\n` +
+        `- duration : ${Func.toDate(metadata.duration)}\n` +
+        `- views : ${Func.h2k(metadata.view_count)}\n` +
+        `- likes : ${Func.h2k(metadata.like_count)}\n` +
+        `- description : ${metadata.description || ''}`
+
+      await ctx.reply({
+        image: { url: metadata.thumbnail },
+        caption
+      })
+
+      await ctx.reply({
+        audio: { url: download.download_url },
+        mimetype: 'audio/mpeg',
+        caption
+      })
+
+    } catch (e) {
+      console.log(e.message)
+      ctx.reply(config.msg.error)
+    }
+  }
+}
