cafd1d450bf7fe2ca5c211461dacddfe66a9e05e
skyzxu <122117174+itsmekaa@users.noreply.github.com>
2026-06-19 17:31:03 +0700

    Update antilink.js

--- plugins/event/antilink.js
+++ plugins/event/antilink.js
@@ -1,6 +1,6 @@
 export const run = {
   event: async (ctx, { sock }) => {
-    if (!ctx.isGroup || !ctx.text) return false
+    if (!ctx.isGroup || !ctx.text || !db.groups[ctx.chat].antilink) return false
 
     const url = Func.validUrl(ctx.text, 'whatsapp.com')
 
