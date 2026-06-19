c6118d0053031ff8af21ddffe41adaac3c953f96
skyzxu <122117174+itsmekaa@users.noreply.github.com>
2026-06-19 21:09:21 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,11 +1,20 @@
+import fs from 'fs'
+import path from 'path'
 import moment from 'moment-timezone'
+
+const pkg = JSON.parse(
+  fs.readFileSync(
+    path.resolve(process.cwd(), 'package.json'),
+    'utf8'
+  )
+)
 
 export const run = {
   cmd: ['menu'],
   hidden: ['m'],
   run: async (ctx) => {
     const categories = {}
-    
+
     for (const [_, plugin] of globalThis.plugins.entries()) {
       if (plugin.category && plugin.cmd) {
         if (!categories[plugin.category]) categories[plugin.category] = []
@@ -14,16 +23,24 @@ export const run = {
     }
 
     const time = moment().tz(config.tz).format('HH:mm:ss')
-    let text = `Hi *${ctx.pushName}* 👋\n\n`
-    text += `◦ Prefix : [ ${config.prefix.join(', ')} ]\n`
-    text += `◦ Time : ${time}\n\n`
+
+    let text = `Hi @${ctx.sender.split('@')[0]} !\n\n`
+    text += '`Bot Info`\n'
+    text += `- Name : ${pkg.name}\n`
+    text += `- Prefix : [ ${config.prefix.join(', ')} ]\n`
+    text += `- Time : ${time}\n\n`
 
     for (const cat in categories) {
-      text += `*[ ${cat.toUpperCase()} ]*\n`
-      text += categories[cat].map(cmd => `◦ ${ctx.prefix || config.prefix[0]}${cmd}`).join('\n')
+      text += `\`${cat}\`\n`
+      text += categories[cat]
+        .sort()
+        .map(cmd => `\`\`\`- ${ctx.prefix || config.prefix[0]}${cmd}\`\`\``)
+        .join('\n')
       text += '\n\n'
     }
 
-    ctx.reply(text.trim())
+    await ctx.reply(text.trim(), {
+      mentions: [ctx.sender]
+    })
   }
 }
