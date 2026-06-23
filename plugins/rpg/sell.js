d85dd00a213dc438245c54c902412f587b2b2263
skyzxu <122117174+itsmekaa@users.noreply.github.com>
2026-06-23 22:18:49 +0700

    Update sell.js

--- plugins/rpg/sell.js
+++ plugins/rpg/sell.js
@@ -4,14 +4,14 @@ export const run = {
   run: async (m, { prefix, command }) => {
     const u = db.users[m.sender].rpg
     const [item, qty, price] = (m.text || '').split(' ')
-    const m = db.rpg.market
+    const market = db.rpg.market
 
     if (!item || !qty || !price) return m.reply(Func.usage(prefix, command, 'item qty price'))
     if ((u.inventory[item] || 0) < qty) return m.reply('Item kurang')
 
     u.inventory[item] -= +qty
 
-    m.sell.push({
+    market.sell.push({
       id: Date.now(),
       owner: m.sender,
       item,
