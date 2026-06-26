086239d34ac9b6ece0b58d05d85be924d1875217
itsmekaa <122117174+itsmekaa@users.noreply.github.com>
2026-06-26 19:53:48 +0700

    feat: add sendContact

---
+++ plugins/miscs/owner.js
@@ -0,0 +1,22 @@
+export const run = {
+  cmd: ['owner'],
+  category: 'miscs',
+  run: async (m, { sock }) => {
+    const contacts = config.owner.map(number => {
+      number = String(number).replace(/\D/g, '')
+      const jid = `${number}@s.whatsapp.net`
+      return {
+        name: db.users[jid]?.name || number,
+        number
+      }
+    })
+
+    await sock.sendContact(
+      m.chat,
+      contacts,
+      {
+        quoted: m
+      }
+    )
+  }
+}
