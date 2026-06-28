4873de068d1cc03bc7caf3402fc0e6a406d6a5fd
itsmekaa <122117174+itsmekaa@users.noreply.github.com>
2026-06-28 19:59:50 +0700

    fix: change scrape to api

--- plugins/ai/remini.js
+++ plugins/ai/remini.js
@@ -1,33 +1,30 @@
+import axios from 'axios'
+
 export const run = {
   cmd: ['remini'],
   hidden: ['upscale', 'hd'],
   category: 'ai',
   run: async (m, { prefix, command }) => {
     if (
-      !(
-        m.type === 'imageMessage' ||
-        (m.quoted && m.quoted.type === 'imageMessage')
-      )
-    ) {
-      return m.reply(Func.usage(prefix, command, '(reply / send image)'))
-    }
+      !(m.type === 'imageMessage' || (m.quoted && m.quoted.type === 'imageMessage'))
+    ) return m.reply(Func.usage(prefix, command, '(reply / send image)'))
 
     await m.reply(config.msg.wait)
 
     try {
-      const media =
-        m.quoted && m.quoted.type === 'imageMessage'
-          ? m.quoted
-          : m
+      const media = m.quoted?.type === 'imageMessage' ? m.quoted : m
+      const url = await uploader.uguu(await media.download())
+      const { data } = await axios.get(
+        `${config.api.baseUrl.skyzxu}/api/ai/enhance`,
+        {
+          params: { url, size: 'medium' },
+          responseType: 'arraybuffer'
+        }
+      )
 
-      const buffer = await media.download()
-      const enhanced = await scrape.enhance(buffer, 'medium')
-
-      await m.reply({
-        image: enhanced
-      })
+      await m.reply({ image: data })
     } catch (e) {
-      console.log(e)
+      console.error(e)
       m.reply(config.msg.error)
     }
   }
