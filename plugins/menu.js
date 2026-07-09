9fba7cd4afac260fb77118a529aa9e281c0635fc
skyzxu <122117174+itsmekaa@users.noreply.github.com>
2026-07-09 22:08:17 +0700

    feat(menu): redesign menu with image caption layout

--- plugins/menu.js
+++ plugins/menu.js
@@ -12,7 +12,7 @@ const pkg = JSON.parse(
 export const run = {
   cmd: ['menu'],
   hidden: ['m'],
-  run: async (m) => {
+  run: async (m, { sock }) => {
     const categories = {}
 
     for (const [, plugin] of globalThis.plugins.entries()) {
@@ -25,10 +25,11 @@ export const run = {
     const time = moment().tz(config.tz).format('HH:mm:ss')
 
     let text = `Hi @${m.sender.split('@')[0]} !\n\n`
-    text += '`Bot Info`\n'
-    text += `- Name : ${pkg.name}\n`
-    text += `- Prefix : [ ${config.prefix.join(', ')} ]\n`
-    text += `- Time : ${time}\n\n`
+    text += '`bot info`\n'
+    text += `- name : ${pkg.name}\n`
+    text += `- version : ${pkg.version}\n`
+    text += `- prefix : [ ${config.prefix.join(', ')} ]\n`
+    text += `- time : ${time}\n\n`
 
     for (const cat of Object.keys(categories).sort()) {
       text += `*- menu ${cat}*\n`
@@ -43,8 +44,24 @@ export const run = {
       text += '\n'
     }
 
-    await m.reply(text.trim(), {
-      mentions: [m.sender]
-    })
+    await sock.sendMessage(
+      m.chat,
+      {
+        image: {
+          url: 'media/image/icon.jpg'
+        },
+        caption: text.trim(),
+        footer: pkg.name,
+        offerText: pkg.version,
+        offerCode: Func.createId(),
+        offerExpiration: Date.now() + 3_600_000,
+        nativeFlow: [],
+        interactiveAsTemplate: false,
+        mentions: [m.sender]
+      },
+      {
+        quoted: m
+      }
+    )
   }
 }
