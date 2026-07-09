fec6fe048273d559e4d4a5db39eb6dd3b8f62de2
itsmekaa <122117174+itsmekaa@users.noreply.github.com>
2026-07-09 22:36:05 +0700

    feat: add new feature

---
+++ plugins/group/wame.js
@@ -0,0 +1,14 @@
+export const run = {
+    cmd: ['wame'],
+    category: 'group',
+    settings: {
+        group: true
+    },
+    run: async (m, {
+        text
+    }) => {
+        const number = (m.quoted ? m.quoted.sender : m.sender).split('@')[0];
+        const chat = text || 'hai';
+        m.reply(`https://wa.me/${number}?text=${encodeURIComponent(chat)}`);
+    }
+}
