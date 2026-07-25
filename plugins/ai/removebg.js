2fe87f4b70bb91168cdc1e92be557c5f76696e89
skyzxu <122117174+itsmekaa@users.noreply.github.com>
2026-07-25 19:57:25 +0700

    feat(ai): add removebg command

---
+++ plugins/ai/removebg.js
@@ -0,0 +1,33 @@
+export const run = {
+  cmd: ['removebg'],
+  hidden: ['rmbg', 'nobg'],
+  category: 'ai',
+  description: 'reply / send image',
+  run: async (m, { prefix, command }) => {
+    if (
+      !(
+        m.type === 'imageMessage' ||
+        (m.quoted && m.quoted.type === 'imageMessage')
+      )
+    )
+      return m.reply(Func.usage(prefix, command, '(reply / send image)'))
+
+    m.react(config.emoji)
+
+    try {
+      const media =
+        m.quoted?.type === 'imageMessage' ? m.quoted : m
+
+      await m.reply({
+        image: await Func.fetchBuffer(
+          `${config.api.baseUrl.zeline}/api/ai/removebg?url=${
+            await uploader.uguu(await media.download())
+          }&key=${config.api.key.zeline}`
+        )
+      })
+    } catch (e) {
+      console.error(e)
+      throw e
+    }
+  }
+}
